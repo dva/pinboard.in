fbe1e9d9961e66b7674ab12cbfa27aebded81a03
Denis A <denisva@gmail.com>
2013-03-01 00:44:55 +0200

    fix with updated packed version

--- popup.js
+++ popup.js
@@ -1,5 +1,3 @@
-// chrome.tabs.create({"url":"https://pinboard.in/add/","selected":true})
-
 chrome.tabs.getSelected(null , function(tab) {
     var iframe = document.getElementById("piframe");
     iframe.src = 'https://pinboard.in/add?url='+
